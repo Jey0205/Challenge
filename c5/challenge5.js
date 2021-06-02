fc880fb0470834df450cdc4348aae0981c48efec
Jey0205 <daedra02@gmail.com>
2021-06-02 03:26:03 -0700

    challenge 5 revisi

--- c5/challenge5.js
+++ c5/challenge5.js
@@ -1,7 +1,11 @@
 function stringManipulation(word) {
-    word = 'ayam \n ebeknyo'
-console.log(word)
+    if( word.charAt(0) == 'a'|| word.charAt(0) == 'i'||word.charAt(0) == 'u'||word.charAt(0) == 'e'||word.charAt(0) == 'o')
+    console.log(word)
+    else{
+        console.log(word.slice(1) + 'nyo')
+    }
 }
+
 
 stringManipulation('ayam') //ayam
 stringManipulation('bebek')//ebeknyo
