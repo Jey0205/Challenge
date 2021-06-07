c10afc5314e7bfbdceff9960980cfb09b956e0f5
Jey0205 <daedra02@gmail.com>
2021-06-07 14:05:45 +0700

    challenge 11 revisi

---
+++ c12/challenge12.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const readline = require('readline');
+const json = JSON.parse(fs.readFileSync("./data.json" , 'utf-8'))
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Tebakan:"
+})
+console.log('Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!')
+console.log(' ')
+
+let a = 0
+
+console.log(json[a].pertanyaan)
+
+rl.prompt()
+rl.on('line', (tebakan) => {
+    if(tebakan == json[a].jawaban){
+        console.log('Selamat Anda Benar!')
+        console.log(' ')
+        a++
+        if(a == 3){
+            console.log('Hore Anda Menang!!')
+            console.log(' ')
+            process.resume()
+        }
+        console.log(json[a].pertanyaan)
+    }else{
+        console.log('wkwkwkwkwk, Anda kurang beruntung!')
+        console.log(' ')
+    }
+    rl.prompt()
+})
+
